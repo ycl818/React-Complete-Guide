d69cf2d78cd39348961dffc94b0c4e557c2e8eb8
ycl818 <kevinyichienlee@gmail.com>
2023-04-24 17:56:58 +0800

    add re-usable button and card

--- 8_project/src/components/Users/AddUser.js
+++ 8_project/src/components/Users/AddUser.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
 
 const AddUser = () => {
   const adduUserHandler = (event) => {
@@ -8,13 +9,13 @@ const AddUser = () => {
   };
 
   return (
-    <Card>
+    <Card className={classes.input}>
       <form onSubmit={adduUserHandler}>
         <label htmlFor="username">UserName</label>
         <input id="username" type="text" />
         <label htmlFor="age">Age(Years)</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
