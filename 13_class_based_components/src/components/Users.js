2ad04812afa09d70fa8e8d718c9af754e41f3465
yichien <kevinyichienlee@gmail.com>
2023-05-11 23:59:46 +0800

    adjust useState and event to class based

---
+++ 13_class_based_components/src/components/Users.js
@@ -0,0 +1,74 @@
+import { Component } from "react";
+import User from "./User";
+
+import classes from "./Users.module.css";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
+
+class Users extends Component {
+  constructor() {
+    // always group all the states in an object
+    super();
+    this.state = {
+      showUsers: true,
+    };
+  }
+
+  toggleUsersHandler() {
+    // this.state.showUsers = false // NOT!
+    // method  provided by Component class, always take an object
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers };
+    });
+  }
+
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+// const Users = () => {
+//   const [showUsers, setShowUsers] = useState(true);
+
+//   const toggleUsersHandler = () => {
+//     setShowUsers((curState) => !curState);
+//   };
+
+// const usersList = (
+//   <ul>
+//     {DUMMY_USERS.map((user) => (
+//       <User key={user.id} name={user.name} />
+//     ))}
+//   </ul>
+// );
+
+//   return (
+//     <div className={classes.users}>
+//       <button onClick={toggleUsersHandler}>
+//         {showUsers ? "Hide" : "Show"} Users
+//       </button>
+//       {showUsers && usersList}
+//     </div>
+//   );
+// };
+
+export default Users;
