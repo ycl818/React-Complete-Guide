cdcfb151e5ec1ae0af7ce15fcd20694258b35304
yichien <kevinyichienlee@gmail.com>
2023-05-13 21:56:34 +0800

    http hook

--- 15_http_custom_react_hook/src/App.js
+++ 15_http_custom_react_hook/src/App.js
@@ -2,38 +2,30 @@ import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-complete-guide-85a78-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json"
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
+  const transformTasks = (taskobj) => {
+    const loadedTasks = [];
+    for (const taskKey in taskobj) {
+      loadedTasks.push({ id: taskKey, text: taskobj[taskKey].text });
     }
-    setIsLoading(false);
+    setTasks(loadedTasks);
   };
+
+  //傳進去hook的值也要注意 在App rerender會重新create
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp(
+    {
+      url: "https://react-complete-guide-85a78-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+    },
+    transformTasks
+  );
 
   useEffect(() => {
     fetchTasks();
