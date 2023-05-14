271648e09a532f4f21f75239700c6bce66a1dd26
yichien <kevinyichienlee@gmail.com>
2023-05-14 22:56:29 +0800

    post userdata and ordered items into firebase

--- 17_http_and_form_to_food_order_app/src/components/Cart/Cart.js
+++ 17_http_and_form_to_food_order_app/src/components/Cart/Cart.js
@@ -25,6 +25,19 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
+  const submitOrderHandler = (userData) => {
+    fetch(
+      "https://react-complete-guide-7ca20-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items,
+        }),
+      }
+    );
+  };
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
@@ -60,7 +73,9 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
       {!isCheckout && modalActions}
     </Modal>
   );
