38ae968e45769c83b0e90642f11e19daec894660
yichien <kevinyichienlee@gmail.com>
2023-05-16 22:05:50 +0800

    useEffect with Redux : save cart items

--- 19_advanced_redux/src/App.js
+++ 19_advanced_redux/src/App.js
@@ -1,10 +1,23 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
+
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://react-complete-guide-78d89-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+      {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
 
   return (
     <Layout>
