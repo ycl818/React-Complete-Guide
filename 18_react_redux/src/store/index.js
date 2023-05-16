a64c21201a340801c2526e185551fd292395452a
ycl818 <kevinyichienlee@gmail.com>
2023-05-16 17:56:35 +0800

    split file

--- 18_react_redux/src/store/index.js
+++ 18_react_redux/src/store/index.js
@@ -1,33 +1,15 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./slice/authSlice";
-
-const initialState = { counter: 0, showCounter: true };
-const counterSlice = createSlice({
-  name: "counter",
-  initialState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
+import { counterReducer } from "./slice/counterSlice";
 
 const store = configureStore({
   reducer: {
-    counter: counterSlice.reducer,
+    counter: counterReducer,
     authentication: authReducer,
   },
 }); // wants a pointer at reducer
 
+export * from "./slice/counterSlice";
 export * from "./slice/authSlice";
-export const counterActions = counterSlice.actions;
+
 export default store;
