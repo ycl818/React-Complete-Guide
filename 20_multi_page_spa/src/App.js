6dde10ec8e3c4dd11499dd910cbb848e1f2e376b
ycl818 <kevinyichienlee@gmail.com>
2023-05-17 16:42:00 +0800

    different approach to create routes

--- 20_multi_page_spa/src/App.js
+++ 20_multi_page_spa/src/App.js
@@ -1,7 +1,23 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import Home from "./pages/Home";
+import Products from "./pages/Products";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Home />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinitions);
+// const router = createBrowserRouter([
+//   { path: "/", element: <Home /> },
+//   { path: "/products", element: <Products /> },
+// ]);
 
 function App() {
   return <RouterProvider router={router} />;
