f4071f216de53b581b0a69c3d54196bd46044449
yichien <kevinyichienlee@gmail.com>
2023-05-18 23:52:15 +0800

    adding Links for dynamic routes

--- 20_multi_page_spa/src/pages/Products.js
+++ 20_multi_page_spa/src/pages/Products.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ProductDrtailPage from "./ProductDrtailPage";
+import { Link } from "react-router-dom";
 
 const Products = () => {
+  const PRODUCTS = [
+    { id: "p1", title: "product1" },
+    { id: "p2", title: "product2" },
+    { id: "p3", title: "product3" },
+  ];
+
   return (
     <>
       <div>Products</div>
-      <ProductDrtailPage />
+
+      <ul>
+        {PRODUCTS.map((prod) => {
+          return (
+            <li>
+              <Link to={`/products/${prod.id}`}>{prod.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
