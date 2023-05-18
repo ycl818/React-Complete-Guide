f34f3d0b97a67068e39ee4fe80b31823864428fd
yichien <kevinyichienlee@gmail.com>
2023-05-18 23:40:47 +0800

    Navigating programmitially

--- 20_multi_page_spa/src/pages/Home.js
+++ 20_multi_page_spa/src/pages/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate("/products");
+  };
   return (
     <>
       <h1>Home</h1>
       <p>
         Go to <Link to="/products"> the list of products</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
