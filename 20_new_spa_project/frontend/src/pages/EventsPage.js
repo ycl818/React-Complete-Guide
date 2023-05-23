c134eb97c510fafdfb7eba4785eadb3ce2509a31
yichien <kevinyichienlee@gmail.com>
2023-05-23 23:46:07 +0800

    return only response in loader function, react-router-dom help parse json data

--- 20_new_spa_project/frontend/src/pages/EventsPage.js
+++ 20_new_spa_project/frontend/src/pages/EventsPage.js
@@ -3,8 +3,8 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
-
+  const data = useLoaderData();
+  const events = data.events;
   return <EventsList events={events} />;
 }
 
@@ -16,7 +16,6 @@ export async function loader() {
   if (!response.ok) {
     // ...
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
