ca2874ecc7c24104c9260b34dd6dd29031da3120
ycl818 <kevinyichienlee@gmail.com>
2023-05-23 16:27:22 +0800

    store loader function into specific file

--- 20_new_spa_project/frontend/src/pages/EventsPage.js
+++ 20_new_spa_project/frontend/src/pages/EventsPage.js
@@ -1,12 +1,22 @@
-import React from "react";
 import { useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
-const EventsPage = () => {
+function EventsPage() {
   const events = useLoaderData();
 
   return <EventsList events={events} />;
-};
+}
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // ...
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
