a5dee4278d84b46d1f7df41b83213997050a14ba
yichien <kevinyichienlee@gmail.com>
2023-05-23 23:52:12 +0800

    handle fetch error approach 1

--- 20_new_spa_project/frontend/src/pages/EventsPage.js
+++ 20_new_spa_project/frontend/src/pages/EventsPage.js
@@ -5,16 +5,20 @@ import EventsList from "../components/EventsList";
 function EventsPage() {
   const data = useLoaderData();
   const events = data.events;
+
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
   return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/events");
+  const response = await fetch("http://localhost:8080/eventssss");
 
   if (!response.ok) {
-    // ...
+    return { isError: true, message: "Could not fetch events." };
   } else {
     return response;
   }
