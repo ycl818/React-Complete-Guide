3c9f93fa352b1b929a3dd5b565848cdaa53f4d49
yichien <kevinyichienlee@gmail.com>
2023-05-31 00:17:53 +0800

    submit data by action

--- 20_new_spa_project/frontend/src/pages/EventDetailPage.js
+++ 20_new_spa_project/frontend/src/pages/EventDetailPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { json, useRouteLoaderData } from "react-router-dom";
+import { json, redirect, useRouteLoaderData } from "react-router-dom";
 import EventItem from "../components/EventItem";
 
 const EventDetailPage = () => {
@@ -24,4 +24,24 @@ export async function loader({ request, params }) {
   } else {
     return response;
   }
+}
+
+export async function action({ params, request }) {
+  const eventId = params.eventid;
+  const response = await fetch(`http://localhost:8080/events/` + eventId, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw json(
+      {
+        message: "Could not delete event.",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+
+  return redirect("/events");
 }
