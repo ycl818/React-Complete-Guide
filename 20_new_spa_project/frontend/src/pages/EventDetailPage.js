70016746015b9ec116426c694a5181444a24c2d9
yichien <kevinyichienlee@gmail.com>
2023-06-01 00:31:58 +0800

    controlling which data should be deferred

--- 20_new_spa_project/frontend/src/pages/EventDetailPage.js
+++ 20_new_spa_project/frontend/src/pages/EventDetailPage.js
@@ -1,29 +1,79 @@
 import React from "react";
-import { json, redirect, useRouteLoaderData } from "react-router-dom";
+import {
+  json,
+  redirect,
+  useRouteLoaderData,
+  defer,
+  Await,
+} from "react-router-dom";
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 const EventDetailPage = () => {
-  const data = useRouteLoaderData("event-detail");
+  const { event, events } = useRouteLoaderData("event-detail");
 
-  return <EventItem event={data.event} />;
+  return (
+    <>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
+    </>
+  );
 };
 
 export default EventDetailPage;
 
-export async function loader({ request, params }) {
-  console.log("🚀 ~ file: EventDetailPage.js:16 ~ loader ~ params:", params);
-  console.log("🚀 ~ file: EventDetailPage.js:16 ~ loader ~ request:", request);
-  const id = params.eventid;
+async function loadEvent(id) {
   const response = await fetch("http://localhost:8080/events/" + id);
 
   if (!response.ok) {
     throw json(
       { message: "Could not fetch details for selected event." },
-      { status: 500 }
+      {
+        status: 500,
+      }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.event;
   }
+}
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return { isError: true, message: 'Could not fetch events.' };
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+    //   status: 500,
+    // });
+    throw json(
+      { message: "Could not fetch events." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export async function loader({ request, params }) {
+  const id = params.eventid;
+
+  return defer({
+    event: await loadEvent(id),
+    events: loadEvents(),
+  });
 }
 
 export async function action({ params, request }) {
--- 20_new_spa_project/frontend/src/pages/HomePage.js
+++ 20_new_spa_project/frontend/src/pages/HomePage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PageContent from "../components/PageContent";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <PageContent title="Welcome!">
+      <p>Browse all out amazing events!</p>
+    </PageContent>
+  );
 };
 
 export default HomePage;
