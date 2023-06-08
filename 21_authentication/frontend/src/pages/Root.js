b1f620fdbb4c2e992ca06dc7ca7a797d40d43001
yichien <kevinyichienlee@gmail.com>
2023-06-09 00:20:36 +0800

    adding automatic logout

--- 21_authentication/frontend/src/pages/Root.js
+++ 21_authentication/frontend/src/pages/Root.js
@@ -1,9 +1,27 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import {
+  Outlet,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    setTimeout(() => {
+      submit(null, { action: "logout", method: "post" });
+    }, 10 * 1000);
+  }, [token, submit]);
 
   return (
     <>
